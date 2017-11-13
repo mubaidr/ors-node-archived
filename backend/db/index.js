2b1e6a7027ceb03ddb39cd8768f09014a3a3298e
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-11-13 15:41:53 +0500

    update nodemon

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,34 +1,69 @@
 const Sequelize = require('sequelize')
+const SequelizeAuto = require('sequelize-auto')
 const config = require('config')
 const fs = require('fs')
 const path = require('path')
 var models = {}
 
-const sequelize = new Sequelize(config.get('db.name'), config.get('db.username'), config.get('db.password'), {
-  host: config.get('db.host'),
-  dialect: 'mssql',
-  freezeTableName: true,
-  operatorsAliases: false
+generateModels(() => {
+  setupDB()
 })
 
-const dir = path.join(__dirname, 'models/')
-const files = fs.readdirSync(dir)
-files.forEach(file => {
-  var model = getModelName(file)
-  models[model] = sequelize.import(dir + file)
-})
+module.exports = models
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.')
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err)
+/**
+ * Utilities
+ */
+
+function setupDB () {
+  // Setup sequelize
+  const sequelize = new Sequelize(config.get('db.name'), config.get('db.username'), config.get('db.password'), {
+    host: config.get('db.host'),
+    dialect: 'mssql',
+    freezeTableName: true,
+    operatorsAliases: false,
+    timestamps: true
   })
 
-//TODO uncomment this before deploy
-// sequelize.sync()
+  // Import models
+  const dir = path.join(__dirname, 'models/')
+  const files = fs.readdirSync(dir)
+  files.forEach(file => {
+    var model = getModelName(file)
+    models[model] = sequelize.import(dir + file)
+  })
+
+  // Test connection
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.')
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err)
+    })
+
+  // Sync DB schema
+  // sequelize.sync()
+}
+
+function generateModels (callback) {
+  var auto = new SequelizeAuto(config.get('db.name'), config.get('db.username'), config.get('db.password'), {
+    host: config.get('db.host'),
+    dialect: 'mssql',
+    additional: {
+      timestamps: true
+    }
+  })
+
+  auto.run(err => {
+    if (err) throw err
+
+    if (callback) {
+      callback()
+    }
+  })
+}
 
 function getModelName (name) {
   var words = name
@@ -44,5 +79,3 @@ function getModelName (name) {
 
   return model
 }
-
-module.exports = models
