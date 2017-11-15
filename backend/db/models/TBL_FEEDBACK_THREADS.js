ce567c7937f154fbb97e2ce7e5b5ea262e712fce
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-11-15 15:46:09 +0500

    fix dateType in model

--- backend/db/models/TBL_FEEDBACK_THREADS.js
+++ backend/db/models/TBL_FEEDBACK_THREADS.js
@@ -25,7 +25,7 @@ module.exports = function(sequelize, DataTypes) {
 			allowNull: false
 		},
 		SUBMIT_DATE: {
-			type: "SMALLDATETIME",
+			type: DataTypes.DATE,
 			allowNull: false
 		},
 	}, {
