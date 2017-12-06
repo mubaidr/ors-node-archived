a30f46f6282832ba05d1489f222349f21a46f7e5
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-12-06 13:27:46 +0500

    add BCH link

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ORS
 
+[![BCH compliance](https://bettercodehub.com/edge/badge/mubaidr/ors-node?branch=master)](https://bettercodehub.com/)
+
 ## Backend
 
 [readme](./backend/README.md)
--- backend/routes/custom/picture.js
+++ backend/routes/custom/picture.js
@@ -1,9 +1,5 @@
 const express = require('express')
 const router = express.Router({ caseSensitive: true })
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const config = require('config')
-const validator = require('validator')
 
 //TODO: implement picture api
 
